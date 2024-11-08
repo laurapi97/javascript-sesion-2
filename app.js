efc3a65288cf97d3f5f57ae4459ff1303a404df4
Laura Fiona <laurapinzonm7@gmail.com>
2024-11-07 19:55:33 -0500

    Añadiendo el Hola Mundo con javascript, y aprendiendo a imprimir en consola

---
+++ app.js
@@ -0,0 +1,36 @@
+// console.log('Hola Mundo');
+//se pueden definir varias variables separadas por la coma 
+let  a = 10,
+    b = 20,
+    c = 'Hola',
+    d = 'Spiderman',
+    x =  a + b;
+
+// c=20; ESTO NO SE PUEDE HACER
+
+// console.log( x );
+// console.warn( x );
+// console.error( x );
+// console.info( x );
+
+// console.log( 'a', a);
+// console.log( 'b', b);
+// console.log( 'c', c);
+// console.log( 'd', d);
+
+//HACE LO MISMO QUE LAS LÍNEAS DE ARRIBA
+// console.log({a});
+// console.log({b});
+// console.log({c});
+// console.log({d});
+// console.log('%c Mis variables', 'color: green; font-weight: bold');
+c = 'Hola de nuevo';
+const saludo = c + " "+ d;
+
+// console.table({a,b,c,d,x});
+
+
+
+//Depuración
+
+c = 'Hola de nuevo';
