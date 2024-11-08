2c8277aa25ad84cc2248109f257d90c75715bcf8
Laura Fiona <laurapinzonm7@gmail.com>
2024-11-08 15:51:47 -0500

    sesión 2

---
+++ assets/js/alerts.js
@@ -0,0 +1,12 @@
+alert('Hola Mundo');
+
+
+let nombre= prompt('¿Cuál es tu nombre?'); //RECIBE UN VALOR O ARGUMENTO EL VALOR POR DEFECTO DESPUES DE LA COMA
+// función que regresa un strinng
+console.log(nombre);
+console.log('****' + nombre + '****');
+
+const seleccion = confirm ('¿Está seguro de borrar esto?');
+console.log(seleccion);
+
+// console.log(global); //comando usado para backend en la terminal usando node
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,14 @@
-alert('Hola desde app.js');
-
+// alert('Hola desde app.js');
 
 // console.log('Hola Mundo');
 //se pueden definir varias variables separadas por la coma 
 let  a = 10,
-    b = 20,
-    c = 'Hola',
-    d = 'Spiderman',
-    x =  a + b;
+     b = 20,
+     c = 'Hola',
+     d = 'Spiderman',
+     x =  a + b;
 
-const saludo = c + " "+ d;
+const saludo = c + " " + d;
 
 // c=20; ESTO NO SE PUEDE HACER
 
@@ -29,12 +28,17 @@ const saludo = c + " "+ d;
 // console.log({c});
 // console.log({d});
 // console.log('%c Mis variables', 'color: green; font-weight: bold');
-// c = 'Hola de nuevo';
+c = 'Hola de nuevo';
 
 
 
 // console.table({a,b,c,d,x});
 
-var miNombre= 'Fiona';
+// var miNombre= 'Fiona';
+//    let outerWidth =1000000;
+//    const outerHeight =600;
 
 //Depuración F5
+
+
+let miNuevoNombre= 'Fiona'
